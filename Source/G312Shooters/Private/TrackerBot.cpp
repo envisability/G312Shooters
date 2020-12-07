bebe747485d15cf0c19a9bfa23ac0faed2da555c
David Barel <david@envisability.com>
2020-12-06 19:01:42 -0800

    Adding tracke bot

---
+++ Source/G312Shooters/Private/TrackerBot.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+
+#include "G312Shooters/Public/TrackerBot.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "DrawDebugHelpers.h"
+#include "Components/SphereComponent.h"
+#include "Sound/SoundCue.h"
+
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
+
+// Sets default values
+ATrackerBot::ATrackerBot()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Add the mesh and ensure that it would not affect navigation 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCanEverAffectNavigation(false);
+	MeshComp->SetSimulatePhysics(true);
+	RootComponent = MeshComp;
+}
+
+// Called when the game starts or when spawned
+void ATrackerBot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+FVector ATrackerBot::GetNextPathPoint()
+{
+	// Get the player, which serves as the target and find the path to it 
+	ACharacter* PlayerPawn = UGameplayStatics::GetPlayerCharacter(this, 0);
+	// UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
+
+	return FVector();
+}
+
+// Called every frame
+void ATrackerBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/G312Shooters/Public/TrackerBot.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TrackerBot.generated.h"
+
+UCLASS()
+class G312SHOOTERS_API ATrackerBot : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATrackerBot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	// Gets the next path point in the path to the target
+	FVector GetNextPathPoint();
+
+	// Next point in navigation path
+	FVector NextPathPoint;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	float MovementForce;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	bool bUseVelocityChange;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	float RequiredDistanceToTarget;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+};
